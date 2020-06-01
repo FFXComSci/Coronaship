19e7deff9eafd60ab8377f173ed0830d1e36c390
Michael <46978966+michaelinfernus@users.noreply.github.com>
2020-05-31 22:55:36 -0400

    Layout Manager

--- src/gui/BoardLayoutManager.java
+++ src/gui/BoardLayoutManager.java
@@ -1,4 +1,56 @@
 package gui;
 
-public class BoardLayoutManager {
+import java.awt.*;
+
+public class BoardLayoutManager implements LayoutManager2 {
+
+    @Override
+    public void addLayoutComponent(Component component, Object o) {
+
+    }
+
+    @Override
+    public Dimension maximumLayoutSize(Container container) {
+        return null;
+    }
+
+    @Override
+    public float getLayoutAlignmentX(Container container) {
+        return 0;
+    }
+
+    @Override
+    public float getLayoutAlignmentY(Container container) {
+        return 0;
+    }
+
+    @Override
+    public void invalidateLayout(Container container) {
+
+    }
+
+    @Override
+    public void addLayoutComponent(String s, Component component) {
+
+    }
+
+    @Override
+    public void removeLayoutComponent(Component component) {
+
+    }
+
+    @Override
+    public Dimension preferredLayoutSize(Container container) {
+        return null;
+    }
+
+    @Override
+    public Dimension minimumLayoutSize(Container container) {
+        return null;
+    }
+
+    @Override
+    public void layoutContainer(Container container) {
+
+    }
 }
